7cb9218b40cf12378584a5ce1d43f422e4bf895b
darwinsenior <darwinsenior@gmail.com>
2016-02-21 23:08:26 -0600

    adding write result

---
+++ assignment3/write_result.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+
+def write_result(name, y):
+    columns = ['Id', 'Prediction']
+    values = np.zeros(y.size, dtype='int32, int32')
+    values.dtype.names = columns
+    df = pd.DataFrame(values, columns=columns)
+    df['Id'] = np.arange(0, y.size)
+    df['Prediction'] = y
+    df.to_csv(name, sep=',', index=False)
