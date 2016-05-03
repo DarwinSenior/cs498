727c19c7358ff35227bc2fe233698f84575485c9
darwinsenior <darwinsenior@gmail.com>
2016-05-02 21:15:08 -0500

    adding the scale

--- additonal_hw/HW7.R
+++ additonal_hw/HW7.R
@@ -1,5 +1,6 @@
 library('gdata')
 library('glmnet')
+library('grpreg')
 
 tempdata = (read.csv('./Oregon_Met_Data.txt', sep=' ', header=T))
 temp_min = sapply(min(tempdata$SID):max(tempdata$SID), function(i) mean(tempdata$Tmin_deg_C[(tempdata$SID == i) & (tempdata$Tmin_deg_C != 9999)]))
@@ -16,6 +17,9 @@ kernel1 = function(sigma) exp(-outer(1:dim(coords)[1], 1:dim(coords)[1], FUN=eud
 mats1 = do.call('cbind', Map(function(sigma) exp(-spaces^2/(2*sigma^2)), ranges))
 
 model = cv.glmnet(mats1, temp_min, alpha=0)
+model = cv.grpreg(mats1, temp_min, group=rep(1:dim(coords)[1], length(ranges)), family='gaussian')
+print(model$lambda.min)
+print(model$fit$df[model$lambda == model$lambda.min])
 
 longs = seq(min(coords[,1]), max(coords[,1]), length=100)
 lats = seq(min(coords[,2]), max(coords[,2]), length=100)
